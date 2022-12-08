4514d44a9711fc27a7a273a5bdfc025358564d41
Subidh Khanal <subidhkhanal37@gmail.com>
2022-12-08 21:12:29 +0545

    made-change-to-newsletter-two

--- components/Extra/Newsletter.jsx
+++ components/Extra/Newsletter.jsx
@@ -5,29 +5,27 @@ import styles from '../../styles/Newsletter.module.css'
     return(
         <div className={styles.container}>
             <div className={styles.cardItem}>
-           <Card size="small" title="Subscribe to Mailer" extra={<a href="#">More</a>} className={styles.cardDetails}>
+           <Card size="small" title="Subscribe to Mailer" extra={<img src="https://ocean.go2andaman.com/wp-content/uploads/2020/05/mailer.svg"/>} className={styles.cardDetails}>
             <p>Stay updated with all the excitement that Andamans has to offer</p>
             <div>
-            <input type="email" class="form-control" data-inputmask="" name="fields[email]" placeholder="Email" autocomplete="email" aria-invalid="false" />
+            <input type="email"  placeholder="Email" />
             <button type="submit" class="primary">Subscribe</button>
             </div>
             </Card> 
             </div>
             <div className={styles.cardItem}>
-            <Card size="small" title="Collaborate with us for Blog/Content" extra={<a href="#">More</a>} className={styles.cardDetails}>
+            <Card size="small" title="Collaborate with us for Blog" extra={<img src="https://ocean.go2andaman.com/wp-content/uploads/2020/10/blog.png"/>} className={styles.cardDetails}>
             <p>If you have a flair for writing, write for us and get featured.</p>
             <div>
-            <input type="email" class="form-control" data-inputmask="" name="fields[email]" placeholder="Email" autocomplete="email" aria-invalid="false" />
-            <button type="submit" class="primary">Subscribe</button>
+            <button type="submit" class="primary">FIND OUT HOW</button>
             </div>
             </Card> 
             </div>
             <div className={styles.cardItem}>
-            <Card size="small" title="Be your own tour guide." extra={<a href="#">More</a>} className={styles.cardDetails}>
+            <Card size="small" title="Be your own tour guide." extra={<img src="https://ocean.go2andaman.com/wp-content/uploads/2020/05/tour.svg" />} className={styles.cardDetails}>
             <p>Get lost or never. You decide. Download our handy tour guides to plan your trip.</p>
             <div>
-            <input type="email" class="form-control" data-inputmask="" name="fields[email]" placeholder="Email" autocomplete="email" aria-invalid="false" />
-            <button type="submit" class="primary">Subscribe</button>
+            <button type="submit" class="primary">DOWNLOAD</button>
             </div>
             </Card> 
             </div>

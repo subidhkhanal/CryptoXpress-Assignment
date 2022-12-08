426ea220146a5941fe3944370cdfdd96aec11c53
Subidh Khanal <subidhkhanal37@gmail.com>
2022-12-08 21:42:47 +0545

    almost-complete-change-to-newsletter

--- components/Extra/Newsletter.jsx
+++ components/Extra/Newsletter.jsx
@@ -1,5 +1,7 @@
 import { Card } from 'antd';
 import styles from '../../styles/Newsletter.module.css'
+import { Input,Button} from 'antd';
+
       
    const Newsletter= () => {
     return(
@@ -7,25 +9,25 @@ import styles from '../../styles/Newsletter.module.css'
             <div className={styles.cardItem}>
            <Card size="small" title="Subscribe to Mailer" extra={<img src="https://ocean.go2andaman.com/wp-content/uploads/2020/05/mailer.svg"/>} className={styles.cardDetails}>
             <p>Stay updated with all the excitement that Andamans has to offer</p>
-            <div>
-            <input type="email"  placeholder="Email" />
-            <button type="submit" class="primary">Subscribe</button>
+            <div className={styles.subscribe}>
+            <Input placeholder="Email"/>
+            <Button className={styles.buttons}>Subscribe</Button>
             </div>
             </Card> 
             </div>
             <div className={styles.cardItem}>
             <Card size="small" title="Collaborate with us for Blog" extra={<img src="https://ocean.go2andaman.com/wp-content/uploads/2020/10/blog.png"/>} className={styles.cardDetails}>
             <p>If you have a flair for writing, write for us and get featured.</p>
-            <div>
-            <button type="submit" class="primary">FIND OUT HOW</button>
+            <div className={styles.buttonDiv}>
+            <Button className={styles.buttons}>FIND OUT HOW</Button>
             </div>
             </Card> 
             </div>
             <div className={styles.cardItem}>
             <Card size="small" title="Be your own tour guide." extra={<img src="https://ocean.go2andaman.com/wp-content/uploads/2020/05/tour.svg" />} className={styles.cardDetails}>
             <p>Get lost or never. You decide. Download our handy tour guides to plan your trip.</p>
-            <div>
-            <button type="submit" class="primary">DOWNLOAD</button>
+            <div className={styles.buttonDiv}>
+            <Button className={styles.buttons}>DOWNLOAD</Button>
             </div>
             </Card> 
             </div>

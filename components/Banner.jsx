4fe1fb302c87d392ca24d1b78d973c3a9f091be3
Subidh Khanal <subidhkhanal37@gmail.com>
2022-12-15 18:53:39 +0545

    made-banner-responsive

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -12,7 +12,6 @@ const Banner = () => {
                 </Breadcrumb>
             </div>
             <div className={styles.containerTwo}>
-                    <img className={styles.bannerImage} src="https://www.go2andaman.com/wp-content/uploads/2021/05/Havelock-Banner-Go2andaman.jpeg" />
                 <div className={styles.textOnImage}>
                     <h1 className={styles.heading}>Havelock Island</h1>
                     <h2 className={styles.details}>Travel Guide - Top Things to Do &amp; See, Hotel &amp; Resorts, Restaurants &amp; Cafe and more </h2>
